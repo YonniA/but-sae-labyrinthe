a213617ff9e8ace630faf352e491bbbef433a4b8
sauv0037 <nino.sauvageot@etudiant.univ-reims.fr>
2023-03-09 16:37:29 +0100

    projet readapte sur fichier.py nomme Maze.py et TestMaze.py

---
+++ Maze.py
@@ -0,0 +1,124 @@
+class Maze:
+    """
+    Classe Labyrinthe
+    Représentation sous forme de graphe non-orienté
+    dont chaque sommet est une cellule (un tuple (l,c))
+    et dont la structure est représentée par un dictionnaire
+      - clés : sommets
+      - valeurs : ensemble des sommets voisins accessibles
+    """
+    def __init__(self, height, width, empty=False):
+        """
+        Constructeur d'un labyrinthe de height cellules de haut
+        et de width cellules de large
+        Les voisinages sont initialisés à des ensembles vides
+        Remarque : dans le labyrinthe créé, chaque cellule est complètement emmurée
+        """
+        self.height    = height
+        self.width     = width
+        if not empty:
+            self.neighbors = {(i, j): set() for i in range(height) for j in range(width)}
+        else:
+            self.neighbors = {(i, j): set() for i in range(height) for j in range(width)}
+            for i in range(height):
+                for j in range(width):
+                    if i > 0:
+                        self.neighbors[(i, j)].add((i - 1, j))
+                    if i < height - 1:
+                        self.neighbors[(i, j)].add((i + 1, j))
+                    if j > 0:
+                        self.neighbors[(i, j)].add((i, j - 1))
+                    if j < width - 1:
+                        self.neighbors[(i, j)].add((i, j + 1))
+
+    def info(self):
+        """
+        **NE PAS MODIFIER CETTE MÉTHODE**
+        Affichage des attributs d'un objet 'Maze' (fonction utile pour deboguer)
+        Retour:
+            chaîne (string): description textuelle des attributs de l'objet
+        """
+        txt = "**Informations sur le labyrinthe**\n"
+        txt += f"- Dimensions de la grille : {self.height} x {self.width}\n"
+        txt += "- Voisinages :\n"
+        txt += str(self.neighbors)+"\n"
+        valid = True
+        for c1 in {(i, j) for i in range(self.height) for j in range(self.width)}:
+            for c2 in self.neighbors[c1]:
+                if c1 not in self.neighbors[c2]:
+                    valid = False
+                    break
+            else:
+                continue
+            break
+        txt += "- Structure cohérente\n" if valid else f"- Structure incohérente : {c1} X {c2}\n"
+        return txt
+
+    def __str__(self):
+        """
+        **NE PAS MODIFIER CETTE MÉTHODE**
+        Représentation textuelle d'un objet Maze (en utilisant des caractères ascii)
+        Retour:
+             chaîne (str) : chaîne de caractères représentant le labyrinthe
+        """
+        txt = ""
+        # Première ligne
+        txt += "┏"
+        for j in range(self.width-1):
+            txt += "━━━┳"
+        txt += "━━━┓\n"
+        txt += "┃"
+        for j in range(self.width-1):
+            txt += "   ┃" if (0,j+1) not in self.neighbors[(0,j)] else "    "
+        txt += "   ┃\n"
+        # Lignes normales
+        for i in range(self.height-1):
+            txt += "┣"
+            for j in range(self.width-1):
+                txt += "━━━╋" if (i+1,j) not in self.neighbors[(i,j)] else "   ╋"
+            txt += "━━━┫\n" if (i+1,self.width-1) not in self.neighbors[(i,self.width-1)] else "   ┫\n"
+            txt += "┃"
+            for j in range(self.width):
+                txt += "   ┃" if (i+1,j+1) not in self.neighbors[(i+1,j)] else "    "
+            txt += "\n"
+        # Bas du tableau
+        txt += "┗"
+        for i in range(self.width-1):
+            txt += "━━━┻"
+        txt += "━━━┛\n"
+
+        return txt
+
+    def add_wall(self, c1, c2):
+        # Facultatif : on teste si les sommets sont bien dans le labyrinthe
+        assert 0 <= c1[0] < self.height and \
+            0 <= c1[1] < self.width and \
+            0 <= c2[0] < self.height and \
+            0 <= c2[1] < self.width, \
+            f"Erreur lors de l'ajout d'un mur entre {c1} et {c2} : les coordonnées de sont pas compatibles avec les dimensions du labyrinthe"
+        # Ajout du mur
+        if c2 in self.neighbors[c1]:      # Si c2 est dans les voisines de c1
+            self.neighbors[c1].remove(c2) # on le retire
+        if c1 in self.neighbors[c2]:      # Si c3 est dans les voisines de c2
+            self.neighbors[c2].remove(c1) # on le retire
+
+    def remove_wall(self, c1, c2):
+        assert 0 <= c1[0] < self.height and \
+               0 <= c1[1] < self.width and \
+               0 <= c2[0] < self.height and \
+               0 <= c2[1] < self.width, \
+            f"Erreur lors de l'ajout d'un mur entre {c1} et {c2} : les coordonnées de sont pas compatibles avec les dimensions du labyrinthe"
+        if c2 not in self.neighbors[c1]:
+            self.neighbors[c1].add(c2)
+        if c1 not in self.neighbors[c2]:
+            self.neighbors[c2].add(c1)
+
+    def get_walls(self):
+        walls = []
+        for i in range(self.height):
+            for j in range(self.width):
+                if i < self.height - 1 and (i + 1, j) not in self.neighbors[(i, j)]:
+                    walls.append(((i, j), (i + 1, j)))
+                if j < self.width - 1 and (i, j + 1) not in self.neighbors[(i, j)]:
+                    walls.append(((i, j), (i, j + 1)))
+        return walls
---
+++ TestMaze.py
@@ -0,0 +1,86 @@
+from Maze import *
+
+print("Exemples d’utilisation :")
+laby = Maze(4, 4)
+print(laby.info())
+print("print(laby)")
+print(laby)
+
+print("Cassons quelques murs en redéfinissant manuellement les voisinages des cellules concernées :")
+laby.neighbors = {
+    (0, 0): {(1, 0)},
+    (0, 1): {(0, 2), (1, 1)},
+    (0, 2): {(0, 1), (0, 3)},
+    (0, 3): {(0, 2), (1, 3)},
+    (1, 0): {(2, 0), (0, 0)},
+    (1, 1): {(0, 1), (1, 2)},
+    (1, 2): {(1, 1), (2, 2)},
+    (1, 3): {(2, 3), (0, 3)},
+    (2, 0): {(1, 0), (2, 1), (3, 0)},
+    (2, 1): {(2, 0), (2, 2)},
+    (2, 2): {(1, 2), (2, 1)},
+    (2, 3): {(3, 3), (1, 3)},
+    (3, 0): {(3, 1), (2, 0)},
+    (3, 1): {(3, 2), (3, 0)},
+    (3, 2): {(3, 1)},
+    (3, 3): {(2, 3)}
+}
+
+print(laby)
+
+laby.neighbors[(1,3)].remove((2,3))
+laby.neighbors[(2,3)].remove((1,3))
+print(laby)
+
+laby.neighbors[(1, 3)].add((2, 3))
+laby.neighbors[(2, 3)].add((1, 3))
+print(laby)
+
+laby.neighbors[(1, 3)].remove((2, 3))
+print(laby)
+print(laby.info())
+
+laby.neighbors[(2, 3)].remove((1,3))
+
+c1 = (1, 3)
+c2 = (2, 3)
+if c1 in laby.neighbors[c2] and c2 in laby.neighbors[c1]:
+    print(f"Il n'y a pas de mur entre {c1} et {c2} car elles sont mutuellement voisines")
+elif c1 not in laby.neighbors[c2] and c2 not in laby.neighbors[c1]:
+    print(f"Il y a un mur entre {c1} et {c2} car {c1} n'est pas dans le voisinage de {c2} et {c2} n'est pas dans le voisinage de {c1}")
+else:
+    print(f"Il y a une incohérence de réciprocité des voisinages de {c1} et {c2}")
+
+c1 = (1, 3)
+c2 = (2, 3)
+if c1 in laby.neighbors[c2] and c2 in laby.neighbors[c1]:
+    print(f"{c1} est accessible depuis {c2} et vice-versa")
+elif c1 not in laby.neighbors[c2] and c2 not in laby.neighbors[c1]:
+    print(f"{c1} n'est pas accessible depuis {c2} et vice-versa")
+else:
+    print(f"Il y a une incohérence de réciprocité des voisinages de {c1} et {c2}")
+
+L = []
+for i in range(laby.height):
+    for j in range(laby.width):
+        L.append((i,j))
+print(f"Liste des cellules : \n{L}")
+
+laby = Maze(4, 4, empty = True)
+print(laby)
+
+laby = Maze(4, 4, empty = False)
+print(laby)
+
+laby = Maze(5, 5, empty = True)
+print(laby)
+
+laby.add_wall((0,0), (0,1))
+print(laby)
+
+laby.remove_wall((0, 0), (0, 1))
+print(laby)
+
+print(laby.get_walls())
+laby.add_wall((0,0), (0,1))
+print(laby.get_walls())
